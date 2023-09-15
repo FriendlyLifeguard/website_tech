311db00f60ba94b5bae368e5494aabc8790459a8
Beeyoung <55970530+FriendlyLifeguard@users.noreply.github.com>
2023-09-15 06:47:15 -0700

    please work

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "alankang.xyz",
+	site: "https://website-tech-git-main-friendlylifeguard.vercel.app",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		remarkRehype: { footnoteLabelProperties: { className: [""] } },
