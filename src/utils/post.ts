128a92509914405b823fe15b9c2c9fe47fa2fc5b
FriendlyLifeguard <byungchul0714@gmail.com>
2025-02-06 22:18:01 -0800

    fixed typo on blog #3

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,28 +1,28 @@
 import type { CollectionEntry } from "astro:content";
 
-export function sortMDByDate(posts: Array<CollectionEntry<"post">>) {
-	return posts.sort((a, b) => {
-		const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
-		const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
-		return bDate - aDate;
-	});
+export function sortMDByDate(posts: CollectionEntry<"post">[]) {
+	return posts
+		.filter(post => !post.data.draft)  // Filter out drafts
+		.sort((a, b) => b.data.publishDate.valueOf() - a.data.publishDate.valueOf());
 }
 
-export function getAllTags(posts: Array<CollectionEntry<"post">>) {
-	return posts.flatMap((post) => [...post.data.tags]);
+export function getUniqueTags(posts: CollectionEntry<"post">[]) {
+	const uniqueTags = new Set<string>();
+	posts
+		.filter(post => !post.data.draft)  // Filter out drafts
+		.forEach((post) => {
+			post.data.tags.forEach((tag) => uniqueTags.add(tag));
+		});
+	return Array.from(uniqueTags);
 }
 
-export function getUniqueTags(posts: Array<CollectionEntry<"post">>) {
-	return [...new Set(getAllTags(posts))];
-}
-
-export function getUniqueTagsWithCount(
-	posts: Array<CollectionEntry<"post">>,
-): Array<[string, number]> {
-	return [
-		...getAllTags(posts).reduce(
-			(acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
-			new Map<string, number>(),
-		),
-	].sort((a, b) => b[1] - a[1]);
+export function getUniqueTagsWithCount(posts: CollectionEntry<"post">[]) {
+	return posts
+		.filter(post => !post.data.draft)  // Filter out drafts
+		.reduce((acc, post) => {
+			post.data.tags.forEach((tag) => {
+				acc.set(tag, (acc.get(tag) || 0) + 1);
+			});
+			return acc;
+		}, new Map<string, number>());
 }
